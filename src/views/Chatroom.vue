1dc39dea548d1d727dda5c0acbe5a73117ff4e17
maan <pranijman@gmail.com>
2021-12-07 14:43:33 +0545

    115

--- src/views/Chatroom.vue
+++ src/views/Chatroom.vue
@@ -5,9 +5,24 @@
 </template>
 <script>
 import Navbar from '../components/Navbar.vue';
+import getUser from '../composables/getUser';
+import { watch } from 'vue';
+import { useRouter } from 'vue-router';
 
 export default {
-  components: { Navbar }
+  components: { Navbar },
+  setup(){
+    const router = useRouter();
+    const { user } = getUser();
+
+    watch(user, () => {
+      if(!user.value){
+        router.push({ name: 'Welcome' });
+      }
+    });
+
+    return { user };
+  }
 };
 </script>
 <style>
